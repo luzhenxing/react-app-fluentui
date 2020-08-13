fc9e250e467388b18bfd764a506556d7b8bf0c1b
luzhenxing <luzhenxing_1230@163.com>
2020-08-13 15:37:11 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PrimaryButton } from '@fluentui/react'
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <br/>
+        <PrimaryButton text="text button" />
       </header>
     </div>
   );
